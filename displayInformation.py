4afcd8e2f0204c38e844b68c99068753768c97f3
akasagiphan <akasagi.phan@gmail.com>
2022-02-19 20:59:25 +0700

    Create displayInformation.py
    Practice

---
+++ displayInformation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("CodeGym Online – Hoc lap trinh Python")
+
+
+# In[2]:
+
+
+print("Tên: Phan Nguyễn Thành Trung")
+print("Giới tính: Nam")
+print("Nơi ở: Hồ Chí Minh")
+
