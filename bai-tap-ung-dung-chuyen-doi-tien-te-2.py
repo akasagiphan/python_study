29fee6ac0925619f175da6c107814979127be8d7
akasagiphan <akasagi.phan@gmail.com>
2022-03-09 22:10:47 +0700

    bai-tap-ung-dung-chuyen-doi-tien-te-2
    bai-tap-ung-dung-chuyen-doi-tien-te-2

---
+++ bai-tap-ung-dung-chuyen-doi-tien-te-2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+tien_usd = float(input("Nhập số tiền USD: "))
+ty_gia = float(input("Nhập tỷ giá USD/VND: "))
+print(f"Số tiền {tien_usd} USD quy đổi sang VND với tỷ giá {ty_gia} là: {tien_usd*ty_gia}")
+
