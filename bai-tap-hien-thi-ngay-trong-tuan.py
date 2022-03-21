cc71a578aee700840477d80b4775e2feeac7c155
akasagiphan <akasagi.phan@gmail.com>
2022-03-21 22:36:37 +0700

    bai-tap-hien-thi-ngay-trong-tuan
    bai-tap-hien-thi-ngay-trong-tuan

---
+++ bai-tap-hien-thi-ngay-trong-tuan.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+day = int(input("Nhập một số từ 1-7: "))
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+else:
+    print("Error, out of range!")
+
